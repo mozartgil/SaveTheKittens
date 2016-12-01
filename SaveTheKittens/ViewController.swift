b3df66afed8c72e8e3bbfeeeac224adeaaa891c0
Mozart Gil <mozartgil@gmail.com>
2016-12-01 13:55:54 +1100

    message save the kittens now

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Save all the Kittens, now!")
     }
 
     override func didReceiveMemoryWarning() {
