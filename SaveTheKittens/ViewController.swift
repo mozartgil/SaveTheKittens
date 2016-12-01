70773036683d98a705db71bb1a2f8c133a69a092
Mozart Gil <mozartgil@gmail.com>
2016-12-01 14:22:28 +1100

    now we are talking

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Yes, that's it
         print("Save all the Kittens, now!")
     }
 
