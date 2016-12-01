99b013315a9f7689e181731af13765fc601bbef5
Mozart Gil <mozartgil@gmail.com>
2016-12-01 18:27:54 +1100

    made a pointless change
    for no apparent reason

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Yes, that's it. TEMP on Master?
+        // new-test branch
         print("Save all the Kittens, now!")
     }
 
