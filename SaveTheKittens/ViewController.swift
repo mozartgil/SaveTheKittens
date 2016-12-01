167b58b19ff57c024c1affeffedac9333cbd504c
Mozart Gil <mozartgil@gmail.com>
2016-12-01 14:32:57 +1100

    temp on master

--- SaveTheKittens/ViewController.swift
+++ SaveTheKittens/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Yes, that's it. TEMP
+        // Yes, that's it. TEMP on Master?
         print("Save all the Kittens, now!")
     }
 
